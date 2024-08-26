8ab26bcb6d3bb85d91d1471ae266311ab33db582
Eldor <eldorforprogramming@gmail.com>
2024-08-26 18:42:21 +0500

    Navbar component completed

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import Navbar from '@/components/Navbar.vue';
 </script>
 
 <template>
-    <RouterView />
+    <div class="min-h-screen bg-[#E7F6FF] p-[85px]">
+        <div class="bg-white rounded-[20px] min-h-full">
+            <Navbar />
+            <RouterView />
+        </div>
+    </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,95 @@
+<script setup>
+import { RouterLink, useRoute } from 'vue-router';
+import { Icon } from '@iconify/vue';
+
+const isActiveLink = (routePath) => {
+    const route = useRoute();
+    return route.path === routePath;
+};
+</script>
+
+<template>
+    <div class="flex justify-between container p-11 border-b min-w-full">
+        <div class="flex items-center">
+            <RouterLink to="/">
+                <img
+                    alt="Logo"
+                    aria-label="Go to home page"
+                    class="w-10 h-10 object-contain mr-4"
+                    src="@/assets/icons/logo.png"
+                />
+            </RouterLink>
+            <div>
+                <h1 class="uppercase text-xl font-bold">vue sneakers</h1>
+                <p class="text-gray-400 text-sm">Магазин лучших кроссовок</p>
+            </div>
+        </div>
+        <div class="flex items-center gap-8">
+            <RouterLink
+                to="/cart"
+                :class="[
+                    'flex',
+                    'items-center',
+                    'gap-2.5',
+                    'text-sm',
+                    'text-gray-400',
+                    'transition',
+                    'duration-200',
+                    'cursor-pointer',
+                    'hover:text-gray-600',
+                    isActiveLink('/cart') ? 'text-gray-600' : '',
+                ]"
+            >
+                <Icon
+                    icon="prime:shopping-cart"
+                    class="text-xl"
+                />
+                <span>0 руб.</span>
+            </RouterLink>
+            <RouterLink
+                to="/favorites"
+                :class="[
+                    'flex',
+                    'items-center',
+                    'gap-2.5',
+                    'text-sm',
+                    'text-gray-400',
+                    'transition',
+                    'duration-200',
+                    'cursor-pointer',
+                    'hover:text-gray-600',
+                    isActiveLink('/favorites') ? 'text-gray-600' : '',
+                ]"
+            >
+                <Icon
+                    icon="prime:heart"
+                    class="text-xl"
+                />
+                <span>Закладки</span>
+            </RouterLink>
+            <RouterLink
+                to="/profile"
+                :class="[
+                    'flex',
+                    'items-center',
+                    'gap-2.5',
+                    'text-sm',
+                    'text-gray-400',
+                    'transition',
+                    'duration-200',
+                    'cursor-pointer',
+                    'hover:text-gray-600',
+                    isActiveLink('/profile') ? 'text-gray-600' : '',
+                ]"
+            >
+                <Icon
+                    icon="gg:profile"
+                    class="text-xl"
+                />
+                <span>Профиль</span>
+            </RouterLink>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
