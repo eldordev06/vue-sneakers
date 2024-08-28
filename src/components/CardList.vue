ce74d1b0bb247fc37a5aaf2fe28779e8eb1fb4ff
Eldor <eldorforprogramming@gmail.com>
2024-08-28 13:53:32 +0500

    layout completed

---
+++ src/components/CardList.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Card from '@/components/Card.vue';
+</script>
+
+<template>
+    <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 pb-5">
+        <Card
+            v-for="n in [0, 0, 0, 0, 0, 0]"
+            :is-favorite="n"
+        />
+    </div>
+</template>
+
+<style scoped></style>
