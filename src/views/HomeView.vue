47df0495dcf920368b7d4834ed14c225c4162375
Eldor <eldorforprogramming@gmail.com>
2024-08-28 13:49:48 +0500

    Cardlist component included & heading and search bar without functionality and design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+import CardList from '@/components/CardList.vue';
+</script>
 
 <template>
-    <h1>Home View</h1>
+    <main class="px-10 sm:px-[60px]">
+        <div
+            class="flex flex-col md:flex-row gap-3 items-center md:justify-between py-10"
+        >
+            <h1 class="text-3xl font-bold text-center">Все кроссовки</h1>
+            <div class="flex border">
+                <img
+                    src="@/assets/icons/search.svg"
+                    alt="Search icon"
+                />
+                <input type="text" />
+            </div>
+        </div>
+        <CardList />
+    </main>
 </template>
 
 <style scoped></style>
