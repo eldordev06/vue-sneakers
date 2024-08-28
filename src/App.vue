c361148b2dc73f3e933adad820be470a3b3db414
Eldor <eldorforprogramming@gmail.com>
2024-08-28 13:46:40 +0500

    Responsive

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@ import Navbar from '@/components/Navbar.vue';
 </script>
 
 <template>
-    <div class="min-h-screen bg-[#E7F6FF] p-[85px]">
-        <div class="bg-white rounded-[20px] min-h-full">
+    <div class="min-h-screen bg-[#E7F6FF] p-4 sm:p-[85px]">
+        <div class="bg-white rounded-[20px] min-h-screen container">
             <Navbar />
             <RouterView />
         </div>
