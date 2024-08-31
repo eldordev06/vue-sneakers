eb0ea27c0a4516cc6f7ea9d994d55ea7cc61e694
Eldor <eldorforprogramming@gmail.com>
2024-08-31 23:10:47 +0500

    card-shadow class added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
         extend: {
+            boxShadow: {
+                'card-shadow': '0 10px 30px hsla(0, 0%, 0%, 0.05)',
+            },
             container: {
                 center: true,
             },
