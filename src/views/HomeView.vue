b482cbc6d33b0a23c07276cffde358e0d86365a4
Eldor <eldorforprogramming@gmail.com>
2024-08-31 22:33:29 +0500

    input styling completed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,16 +8,29 @@ import CardList from '@/components/CardList.vue';
             class="flex flex-col md:flex-row gap-3 items-center md:justify-between py-10"
         >
             <h1 class="text-3xl font-bold text-center">Все кроссовки</h1>
-            <div class="flex border">
+            <label
+                for="search"
+                class="flex border-2 items-center gap-3.5 px-5 h-11 rounded-xl input-focus cursor-pointer"
+            >
                 <img
                     src="@/assets/icons/search.svg"
                     alt="Search icon"
                 />
-                <input type="text" />
-            </div>
+                <input
+                    type="text"
+                    name="search"
+                    id="search"
+                    placeholder="Поиск..."
+                    class="outline-none text-gray-500"
+                />
+            </label>
         </div>
         <CardList />
     </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.input-focus:has(input:focus) {
+    border-color: #c5c5c5;
+}
+</style>
