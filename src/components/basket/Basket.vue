06c401c9cf1e3d3101738253bb7e0f9ae5a4d3e0
Eldor <eldorforprogramming@gmail.com>
2024-09-04 11:03:24 +0500

    completed

---
+++ src/components/basket/Basket.vue
@@ -0,0 +1,15 @@
+<script setup>
+import BasketItem from '@/components/basket/BasketItem.vue';
+</script>
+
+<template>
+    <div class="flex flex-col gap-5 overflow-y-auto">
+        <BasketItem />
+        <BasketItem />
+        <BasketItem />
+        <BasketItem />
+        <BasketItem />
+    </div>
+</template>
+
+<style scoped></style>
