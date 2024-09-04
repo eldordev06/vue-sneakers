4a467ff9eeebca9715ef33bb9fd70b54eda4e678
Eldor <eldorforprogramming@gmail.com>
2024-09-04 11:03:38 +0500

    completed

---
+++ src/components/drawer/DrawerHead.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+</script>
+
+<template>
+    <div class="mb-[30px] flex items-center gap-2">
+        <Icon
+            icon="formkit:arrowleft"
+            class="text-gray-500 transition hover:text-black hover:-translate-x-1 active:-translate-x-2"
+        />
+        <h1 class="text-2xl font-bold select-none">DRAWER</h1>
+    </div>
+</template>
+
+<style scoped></style>
