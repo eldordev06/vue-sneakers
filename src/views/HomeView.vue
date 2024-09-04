5a0af8f4b00cddb2e768475f523ca93c6615043e
Eldor <eldorforprogramming@gmail.com>
2024-09-04 23:09:32 +0500

    select added for sorting (layout only)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,25 +5,36 @@ import CardList from '@/components/card/CardList.vue';
 <template>
     <main class="px-10 sm:px-[60px]">
         <div
-            class="flex flex-col md:flex-row gap-3 items-center md:justify-between py-10"
+            class="flex flex-col lg:flex-row gap-3 items-center md:justify-between py-10"
         >
             <h1 class="text-3xl font-bold text-center">Все кроссовки</h1>
-            <label
-                for="search"
-                class="flex border-2 items-center gap-3.5 px-5 h-11 rounded-xl input-focus cursor-pointer"
-            >
-                <img
-                    src="@/assets/icons/search.svg"
-                    alt="Search icon"
-                />
-                <input
-                    type="text"
-                    name="search"
-                    id="search"
-                    placeholder="Поиск..."
-                    class="outline-none text-gray-500"
-                />
-            </label>
+            <div class="flex flex-col md:flex-row gap-3 items-center">
+                <select
+                    name="sort"
+                    id="sort"
+                    class="border-2 rounded-xl text-gray-500 outline-none h-11 px-5"
+                >
+                    <option value="title">По названию</option>
+                    <option value="cheap">По цене (дешёвые)</option>
+                    <option value="expensive">По цене (дорогие)</option>
+                </select>
+                <label
+                    for="search"
+                    class="flex border-2 items-center gap-5 px-5 h-11 rounded-xl input-focus cursor-pointer"
+                >
+                    <img
+                        src="@/assets/icons/search.svg"
+                        alt="Search icon"
+                    />
+                    <input
+                        type="text"
+                        name="search"
+                        id="search"
+                        placeholder="Поиск..."
+                        class="outline-none text-gray-500"
+                    />
+                </label>
+            </div>
         </div>
         <CardList />
     </main>
