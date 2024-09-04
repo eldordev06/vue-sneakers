a88411c54ca60a207b2d5ef4bdf8feaff7ed0758
Eldor <eldorforprogramming@gmail.com>
2024-09-04 10:39:48 +0500

    drawer grid rows & drawer bg color classes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
         extend: {
+            gridTemplateRows: {
+                drawer: 'auto 1fr auto',
+            },
             boxShadow: {
                 'card-shadow': '0 10px 30px hsla(0, 0%, 0%, 0.05)',
+            },
+            colors: {
+                'drawer-bg': 'hsla(0, 0%, 0%, 0.5)',
             },
             container: {
                 center: true,
