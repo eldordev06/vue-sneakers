beaeeeb04473141ee98ee6fca56d5ac5203cfd80
Eldor <eldorforprogramming@gmail.com>
2024-09-17 17:15:48 +0500

    arrow-left hover and active design fixed

--- src/components/drawer/DrawerHead.vue
+++ src/components/drawer/DrawerHead.vue
@@ -6,7 +6,7 @@ import { Icon } from '@iconify/vue';
     <div class="mb-[30px] flex items-center gap-2">
         <Icon
             icon="formkit:arrowleft"
-            class="text-gray-500 transition hover:text-black hover:-translate-x-1 active:-translate-x-2"
+            class="text-gray-500 cursor-pointer transition lg:hover:text-black lg:hover:-translate-x-1 active:-translate-x-2 active:text-black"
         />
         <h1 class="text-2xl font-bold select-none">DRAWER</h1>
     </div>
