ce1f16779164167dc8ef45577d47822372dac4ae
Eldor <eldorforprogramming@gmail.com>
2024-09-18 17:34:48 +0500

    button added for accessibility

--- src/components/drawer/DrawerHead.vue
+++ src/components/drawer/DrawerHead.vue
@@ -6,11 +6,13 @@ defineProps({ closeAndAnimateDrawer: Function });
 
 <template>
     <div class="mb-[30px] flex items-center gap-2">
-        <Icon
-            icon="formkit:arrowleft"
-            class="text-gray-500 cursor-pointer transition lg:hover:text-black lg:hover:-translate-x-1 active:-translate-x-2 active:text-black"
-            @click="closeAndAnimateDrawer"
-        />
+        <button aria-label="close cart">
+            <Icon
+                icon="formkit:arrowleft"
+                class="text-gray-500 cursor-pointer transition lg:hover:text-black lg:hover:-translate-x-1 active:-translate-x-2 active:text-black"
+                @click="closeAndAnimateDrawer"
+            />
+        </button>
         <h1 class="text-2xl font-bold select-none">DRAWER</h1>
     </div>
 </template>
