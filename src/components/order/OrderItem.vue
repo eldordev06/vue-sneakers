b1cf3495afee6ae83409437be8cfc9ffa4cc4c5d
Eldor <eldorforprogramming@gmail.com>
2024-10-25 20:30:02 +0500

    ProfileView.vue created. Displaying & manipulating (deleting only) orders.

---
+++ src/components/order/OrderItem.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({ product: Object });
+</script>
+
+<template>
+    <div
+        class="pt-5 pb-8 px-[30px] flex flex-col items-center justify-between rounded-3xl border border-gray-200 transition lg:hover:-translate-y-2.5 lg:hover:shadow-card-shadow"
+    >
+        <img
+            :src="`/images${product.imageUrl}`"
+            alt="Sneakers"
+            class="w-3/4 object-contain"
+        />
+        <h3 class="my-3.5">{{ product.title }}</h3>
+        <div class="flex justify-between items-center w-full">
+            <div class="flex flex-col">
+                <span class="text-gray-400 text-sm">Цена:</span>
+                <strong>
+                    {{ product.price.toLocaleString().replace(',', ' ') }} руб.
+                </strong>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { inject } from 'vue';
+import InfoBlock from '@/components/InfoBlock.vue';
+import OrderList from '@/components/order/orderList.vue';
+
+const orders = inject('orders');
+</script>
+
+<template>
+    <div v-if="orders.length">
+        <h1 class="pt-5 text-3xl font-bold text-center md:text-start">
+            Мои заказы
+        </h1>
+        <OrderList
+            v-for="order in orders"
+            :key="order.id"
+            :order="order"
+            :is-last-order="orders.indexOf(order) + 1 === orders.length"
+        />
+    </div>
+    <div
+        v-else
+        class="mt-20"
+    >
+        <InfoBlock
+            image-url="/icons/emoji-2.png"
+            image-width="70"
+            image-height="70"
+            image-descr="Sad face emoji."
+            title="У вас нет заказов"
+            descr="Вы нищеброд? Оформите хотя бы один заказ."
+        />
+    </div>
+</template>
+
+<style scoped></style>
